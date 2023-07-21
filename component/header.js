c70f0ce32f90246b32d0de7c06a5dac4c786c7dc
dovanda1997 <dovandaa976@gmail.com>
2023-07-21 20:37:29 +0700

    fix7

--- component/header.js
+++ component/header.js
@@ -7,7 +7,7 @@ header.innerHTML=`
 		<i class="fa-solid fa-bars"></i>
 		<span>Menu</span>
 	</button>
-	<a href="./index.html" class="logo"><img src="./image/logo-donggia-gold.png"></a>
+	<a href="./index.html" class="logo"><img src="/image/logo-donggia-gold.png"></a>
 	<button class="projects">
 		<span><a href="./duan/duan.html">Dự án</a></span>
 		<i class="fa-solid fa-ellipsis-vertical"></i>
